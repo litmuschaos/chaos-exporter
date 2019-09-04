c806984e59f150c7f206cb5355170974e0e1bb98
rajdas <mail.rajdas@gmail.com>
2019-09-04 16:12:14 +0530

    changes

--- tests/bdd/bdd_test.go
+++ tests/bdd/bdd_test.go
@@ -2,16 +2,15 @@ package bdd
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"testing"
+	"time"
 
 	"github.com/litmuschaos/chaos-exporter/pkg/chaosmetrics"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -28,27 +27,10 @@ var _ = BeforeSuite(func() {
 	os.Setenv("APP_NAMESPACE", "litmus")     // set enc ns to litmus
 	os.Setenv("APP_UUID", "1234")
 
-	// Below 3 commands are creating custom resource defination and custom resource chaosEngine under litmus namespace
-	crdcmd := exec.Command("kubectl", "create", "-f", "\"https://raw.githubusercontent.com/litmuschaos/chaos-operator/master/deploy/crds/chaosengine_crd.yaml\"").Run()
-	nscmd := exec.Command("kubectl", "create", "ns", "litmus").Run()
-	chaosenginecmd := exec.Command("kubectl", "create", "-f", "\"https://raw.githubusercontent.com/litmuschaos/chaos-operator/master/deploy/crds/chaosengine.yaml\"").Run()
+	buildCMD := exec.Command("nohup", "go", "run", "../../cmd/exporter/main.go", "-kubeconfig=/home/"+os.Getenv("HOME")+"/.kube/config", "&").Run()
+	fmt.Println(buildCMD)
+	time.Sleep(3 * time.Second)
 
-	buildCMD := exec.Command("nohup", "go", "run", "../../cmd/exporter/main.go", "-kubeconfig=/home/rajdas/.kube/config", "&").Run()
-	if crdcmd != nil {
-		log.Fatal(crdcmd)
-	}
-
-	if nscmd != nil {
-		log.Fatal(nscmd)
-	}
-
-	if chaosenginecmd != nil {
-		log.Fatal(chaosenginecmd)
-	}
-
-	if buildCMD != nil {
-		log.Fatal(buildCMD)
-	}
 })
 
 var _ = Describe("BDD on chaos-exporter", func() {
@@ -68,6 +50,7 @@ var _ = Describe("BDD on chaos-exporter", func() {
 			if err != nil {
 				Fail(err.Error())
 			}
+			By("Checking Total failed experiments")
 			expTotal, passTotal, failTotal, expMap, err := chaosmetrics.GetLitmusChaosMetrics(config, chaosengine, appNS)
 			if err != nil {
 				Fail(err.Error()) // Unable to get metrics:
@@ -75,7 +58,6 @@ var _ = Describe("BDD on chaos-exporter", func() {
 
 			fmt.Println(expTotal, failTotal, passTotal, expMap)
 
-			// check if failed experiments is 0
 			Expect(failTotal).To(Equal(float64(0)))
 
 		})
@@ -83,8 +65,11 @@ var _ = Describe("BDD on chaos-exporter", func() {
 
 	Context("Curl the prometheus metrics", func() {
 		It("Should return prometheus metrics", func() {
+			fmt.Println(os.Getenv("CHAOSENGINE"))
+			fmt.Println(os.Getenv("APP_NAMESPACE"))
+			fmt.Println(os.Getenv("APP_UUID"))
 
-			resp, err := http.Get("127.0.0.1:8080/metrics")
+			resp, err := http.Get("http://127.0.0.1:8080/metrics")
 			Expect(err).To(BeNil())
 			defer resp.Body.Close()
 		})
@@ -92,12 +77,13 @@ var _ = Describe("BDD on chaos-exporter", func() {
 })
 
 var _ = AfterSuite(func() {
-	// command for delete the custom resource definition
-	deletecrdcmd := exec.Command("kubectl", "delete", "crd", "--all").Run()
-	Expect(deletecrdcmd).To(BeNil())
 
-	// command for delete the namespace
-	deletenscmd := exec.Command("kubectl", "delete", "ns", "litmus").Run()
-	Expect(deletenscmd).To(BeNil())
+	By("Deleting chaosengine CRD")
+	ceDeleteCRDs := exec.Command("kubectl", "delete", "crds", "chaosengines.litmuschaos.io").Run()
+	Expect(ceDeleteCRDs).To(BeNil())
+
+	By("Deleting namespace litmus")
+	deleteNS := exec.Command("kubectl", "delete", "ns", "litmus").Run()
+	Expect(deleteNS).To(BeNil())
 
 })
