171fb32d087b98b0771d32f8be3483ffb21082b1
chandan kumar <chandan.kumar@mayadata.io>
2019-10-01 20:17:36 +0530

    Split the chaosResult calculation function
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>

--- controller/scrape.go
+++ controller/scrape.go
@@ -15,26 +15,30 @@ import (
 var chaosExperimentList []string
 
 // Holds a map of experiment: result
-var chaosresultmap map[string]string
-
-// Holds a map of experiment: numeric representation(result)
-var statusmap map[string]float64
+var chaosResultMap map[string]string
 
 // Holds a lookup of result: numericValue
-var numericstatus = map[string]float64{
+var numericStatus = map[string]float64{
 	"not-executed": 0,
 	"running":      1,
 	"fail":         2,
 	"pass":         3,
 }
 
+// ChaosExpResult...
+type ChaosExpResult struct {
+	TotalExpCount  float64
+	TotalPassedExp float64
+	TotalFailedExp float64
+	StatusMap      map[string]float64
+}
+
 // Utility fn to return numeric value for a result
-func statusConv(expstatus string) (numeric float64) {
-	if numeric, ok := numericstatus[expstatus]; ok {
+func statusConversion(expStatus string) (numeric float64) {
+	if numeric, ok := numericStatus[expStatus]; ok {
 		return numeric
 	}
-	//return 127
-	return 0
+	return numericStatus["not-executed"]
 }
 
 // GetLitmusChaosMetrics returns chaos metrics for a given chaosengine
@@ -54,43 +58,41 @@ func GetLitmusChaosMetrics(config *rest.Config, exporterSpec ExporterSpec) (floa
 		chaosExperimentList = append(chaosExperimentList, element.Name)
 	}
 
-	// Set default values on the chaosresult map before populating w/ actual values
+	// Set default values on the chaosResult map before populating w/ actual values
 
 	for _, test := range chaosExperimentList {
-		chaosresultname := fmt.Sprintf("%s-%s", exporterSpec.ChaosEngine, test)
-		testresultdump, err := clientSet.LitmuschaosV1alpha1().ChaosResults(exporterSpec.AppNS).Get(chaosresultname, metav1.GetOptions{})
+		chaosResultName := fmt.Sprintf("%s-%s", exporterSpec.ChaosEngine, test)
+		testResultDump, err := clientSet.LitmuschaosV1alpha1().ChaosResults(exporterSpec.AppNS).Get(chaosResultName, metav1.GetOptions{})
 		if err != nil {
 			if strings.Contains(err.Error(), "not found") {
 				// lack of result cr indicates experiment not executed
-				chaosresultmap[test] = "not-executed"
+				chaosResultMap[test] = "not-executed"
 			}
-			//return 0, 0, 0, nil, err
+			return 0, 0, 0, nil, err
 		}
-		result := testresultdump.Spec.ExperimentStatus.Verdict
-		//chaosresultmap[chaosresultname] = result
-		chaosresultmap[test] = result
+		result := testResultDump.Spec.ExperimentStatus.Verdict
+		chaosResultMap[test] = result
 	}
 
-	pcount, fcount := 0, 0
-	for _, verdict := range chaosresultmap {
-		if verdict == "pass" {
-			pcount++
-		} else if verdict == "fail" {
-			fcount++
-		}
-	}
-
-	//Map verdict to numerical values {0-notstarted, 1-running, 2-fail, 3-pass}
-	statusmap := make(map[string]float64)
-	for index, status := range chaosresultmap {
-		val := statusConv(status)
-		statusmap[index] = val
-	}
-	fmt.Printf("%+v\n", statusmap)
-
-	totalPassedExp := float64(pcount)
-	totalFailedExp := float64(fcount)
+	chaosResult := calculateChaosResult(chaosResultMap)
+	fmt.Printf("%+v\n", chaosResult.StatusMap)
 	totalExpCount := float64(len(engine.Spec.Experiments))
 
-	return totalExpCount, totalPassedExp, totalFailedExp, statusmap, nil
+	return totalExpCount, chaosResult.TotalPassedExp, chaosResult.TotalFailedExp, chaosResult.StatusMap, nil
+}
+
+// calculateChaosResult will calculate the number of pass and failed experiments
+func calculateChaosResult(chaosResult map[string]string) ChaosExpResult {
+	var cr ChaosExpResult
+
+	for index, status := range chaosResult {
+		if status == "pass" {
+			cr.TotalPassedExp++
+		} else if status == "fail" {
+			cr.TotalFailedExp++
+		}
+		cr.StatusMap[index] = statusConversion(status)
+	}
+
+	return cr
 }
