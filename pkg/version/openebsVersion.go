3d6617966df330aff8f351c2d51f6a1c05a0db60
Rampreeth Ethiraj <rampreeth.ethiraj@grabtaxi.com>
2019-10-02 12:59:49 +0530

    Refactor pkg/version/openebsVersion.go
    Signed-off-by: Rampreeth Ethiraj <ethirajrampreeth@gmail.com>
    Signed-off-by: rampreeth ethiraj <ethirajrampreeth@gmail.com>

--- pkg/version/openebsVersion.go
+++ pkg/version/openebsVersion.go
@@ -9,29 +9,28 @@ import (
 
 var openebsVersion string
 
+// Common check error function
+func Check(msg string, err error){
+	if err != nil {
+		log.info(msg)
+		return "N/A", err
+	}
+}
+
 // GetOpenebsVersion function fetchs the OpenEBS version
 func GetOpenebsVersion(cfg *rest.Config, namespace string) (string, error) {
 	clientSet, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		log.Info("Unable to create the required ClientSet")
-		return "N/A", err
-	}
+	return Check(err)
 	list, err := clientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{
 		LabelSelector: "openebs.io/component-name=maya-apiserver",
 		Limit:         1,
 	})
-	if err != nil {
-		log.Info("Unable to find openebs / maya api-server")
-		return "N/A", err
-	}
+	return Check(err)
 	if len(list.Items) == 0 {
-		log.Info("No resources with labels 'openebs.io/component-name=maya-apiserver' found")
-		return "N/A", err
+		return Check(err)
 	}
 	for _, v := range list.Items {
 		openebsVersion = v.GetLabels()["openebs.io/version"]
 	}
-
 	return openebsVersion, err
-
 }
