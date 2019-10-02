b5bc80aaf97142f7caab93b94eaf0fb0ba80942d
Rampreeth Ethiraj <rampreeth.ethiraj@grabtaxi.com>
2019-10-02 13:47:51 +0530

    Refactor pkg/version/openebsVersion.go
    Signed-off-by: rampreeth ethiraj <ethirajrampreeth@gmail.com>

--- pkg/version/openebsVersion.go
+++ pkg/version/openebsVersion.go
@@ -12,7 +12,7 @@ var openebsVersion string
 // Common check error function
 func Check(msg string, err error){
 	if err != nil {
-		log.info(msg)
+		log.Info(msg)
 		return "N/A", err
 	}
 }
@@ -20,14 +20,17 @@ func Check(msg string, err error){
 // GetOpenebsVersion function fetchs the OpenEBS version
 func GetOpenebsVersion(cfg *rest.Config, namespace string) (string, error) {
 	clientSet, err := kubernetes.NewForConfig(cfg)
-	return Check(err)
+	openebsVersion, err := Check(err)
+	return openebsVersion, err
 	list, err := clientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{
 		LabelSelector: "openebs.io/component-name=maya-apiserver",
 		Limit:         1,
 	})
-	return Check(err)
+	openebsVersion, err := Check(err)
+	return openebsVersion, err
 	if len(list.Items) == 0 {
-		return Check(err)
+		openebsVersion, err := Check(err)
+		return openebsVersion, err
 	}
 	for _, v := range list.Items {
 		openebsVersion = v.GetLabels()["openebs.io/version"]
