2eeedc85d86b63211502af5f4426b92668bcbfee
Rampreeth Ethiraj <rampreeth.ethiraj@grabtaxi.com>
2019-10-04 14:50:23 +0530

    Refactor OpenEbs version.go
    Signed-off-by: rampreeth ethiraj <ethirajrampreeth@gmail.com>

--- pkg/version/openebsVersion.go
+++ pkg/version/openebsVersion.go
@@ -4,6 +4,7 @@ import (
 	log "github.com/Sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/api/core/v1"
 	"k8s.io/client-go/rest"
 )
 
@@ -11,17 +12,17 @@ var openebsVersion = "N/A"
 
 func Check(err error, msg string) {
 	if err != nil {
-		log.info(msg)
+		log.Info(msg)
 	}
 }
 
-func GetClientSet(cfg *rest.Config) {
+func GetClientSet(cfg *rest.Config) (*kubernetes.Clientset, error) {
   clientSet, err := kubernetes.NewForConfig(cfg)
 	Check(err, "Unable to create the required ClientSet")
   return clientSet, err
 }
 
-func ObtainList(clientSet Clientset) {
+func ObtainList(clientSet *kubernetes.Clientset, namespace string) (*v1.PodList, error) {
 	list, err := clientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{
 		LabelSelector: "openebs.io/component-name=maya-apiserver",
 		Limit:         1,
@@ -29,24 +30,31 @@ func ObtainList(clientSet Clientset) {
 	Check(err, "Unable to find openebs / maya api-server")
 	return list, err
 }
+
+func CheckIfEmptyList(list *v1.PodList) bool{
+	if len(list.Items) == 0 {
+		log.Info("No resources with labels 'openebs.io/component-name=maya-apiserver' found")
+		return true
+	}
+	return false
+}
+
 // GetOpenebsVersion function fetchs the OpenEBS version
 func GetOpenebsVersion(cfg *rest.Config, namespace string) (string, error) {
 	clientSet, err := GetClientSet(cfg)
 	if err != nil {
 		return openebsVersion, err
 	}
-	list, err := ObtainList(clientSet)
+	list, err := ObtainList(clientSet, namespace)
 	if err != nil {
 		return  openebsVersion, err
 	}
-	if len(list.Items) == 0 {
-		log.Info("No resources with labels 'openebs.io/component-name=maya-apiserver' found")
+	if (CheckIfEmptyList(list)) {
 		return openebsVersion, err
 	}
 	for _, v := range list.Items {
 		openebsVersion = v.GetLabels()["openebs.io/version"]
 	}
-
 	return openebsVersion, err
 
 }
