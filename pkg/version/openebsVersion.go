fa6b770000f349a5dd3c18a8c784fb30595edd61
Rampreeth Ethiraj <rampreeth.ethiraj@grabtaxi.com>
2019-10-04 11:32:40 +0530

    Refactor pkg version
    Signed-off-by: Rampreeth Ethiraj <ethirajrampreeth@gmail.com>
    Signed-off-by: rampreeth ethiraj <ethirajrampreeth@gmail.com>

--- pkg/version/openebsVersion.go
+++ pkg/version/openebsVersion.go
@@ -7,34 +7,46 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-var openebsVersion string
+var openebsVersion = "N/A"
 
-// Common check error function
-func Check(msg string, err error) {
+func Check(err error, msg string) {
 	if err != nil {
-		log.Info(msg)
-		return "N/A", err
+		log.info(msg)
 	}
 }
 
+func GetClientSet(cfg *rest.Config) {
+  clientSet, err := kubernetes.NewForConfig(cfg)
+	Check(err, "Unable to create the required ClientSet")
+  return clientSet, err
+}
+
+func ObtainList(clientSet Clientset) {
+	list, err := clientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{
+		LabelSelector: "openebs.io/component-name=maya-apiserver",
+		Limit:         1,
+	})
+	Check(err, "Unable to find openebs / maya api-server")
+	return list, err
+}
 // GetOpenebsVersion function fetchs the OpenEBS version
 func GetOpenebsVersion(cfg *rest.Config, namespace string) (string, error) {
-	clientSet, err := kubernetes.NewForConfig(cfg)
-	openebsVersion, err := Check("Unable to create the required ClientSet", err)
+	clientSet, err := GetClientSet(cfg)
 	if err != nil {
-		list, err := clientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{
-			LabelSelector: "openebs.io/component-name=maya-apiserver",
-			Limit:         1,
-		})
-		openebsVersion, err := Check("Unable to find openebs / maya api-server", err)
-		if err != nil {
-			if len(list.Items) == 0 {
-				openebsVersion, err := Check("No resources with labels 'openebs.io/component-name=maya-apiserver' found", err)
-			}
-			for _, v := range list.Items {
-				openebsVersion = v.GetLabels()["openebs.io/version"]
-			}
-		}
+		return openebsVersion, err
 	}
+	list, err := ObtainList(clientSet)
+	if err != nil {
+		return  openebsVersion, err
+	}
+	if len(list.Items) == 0 {
+		log.Info("No resources with labels 'openebs.io/component-name=maya-apiserver' found")
+		return openebsVersion, err
+	}
+	for _, v := range list.Items {
+		openebsVersion = v.GetLabels()["openebs.io/version"]
+	}
+
 	return openebsVersion, err
+
 }
