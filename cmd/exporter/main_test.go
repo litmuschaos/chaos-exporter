519288307a3dc088fb8fb6c7864084e77928d36d
suresh pathipati <psureshkumarece@gmail.com>
2019-10-06 12:51:42 +0530

    GetOpenebsEnv Indendation fix
    Signed-off-by: suresh pathipati <psureshkumarece@gmail.com>

--- cmd/exporter/main_test.go
+++ cmd/exporter/main_test.go
@@ -1,47 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"testing"
-	"os"
+  "fmt"
+  "testing"
+  "os"
 )
 
 // TestChaosExporter is a sample test function
 func TestChaosExporter(t *testing.T) {
-	fmt.Println("..Test Chaos Exporter..")
+  fmt.Println("..Test Chaos Exporter..")
 }
 
 func TestGetOpenebsEnv(t *testing.T) {
-	// Set Enviroment variable
-	os.Setenv("_UTEST_OPENEBS", "utestopenebs")
+  // Set Enviroment variable
+  os.Setenv("_UTEST_OPENEBS", "utestopenebs")
 
-	tests := map[string]struct {
-		inputkey 					string
-		fallbackvalue 		string
-		returnvalue 			string
-	}{
-		"Test Positive-1":{
-			inputkey: 			"_UTEST_OPENEBS", 
-			fallbackvalue: 	"N/A",
-			returnvalue: 		"utestopenebs",
-		},
-		"Test Negative-1":{
-			inputkey: 			"_RUTEST_OPENEBS", 
-			fallbackvalue: 	"rutest_openebs",
-			returnvalue: 		"rutest_openebs",
-		},
-	}
+  tests := map[string]struct {
+    inputkey          string
+    fallbackvalue     string
+    returnvalue       string
+  }{
+    "Test Positive-1":{
+      inputkey:       "_UTEST_OPENEBS", 
+      fallbackvalue:  "N/A",
+      returnvalue:    "utestopenebs",
+    },
+    "Test Negative-1":{
+      inputkey:       "_RUTEST_OPENEBS", 
+      fallbackvalue:  "rutest_openebs",
+      returnvalue:    "rutest_openebs",
+    },
+  }
 
-	for name, mock := range tests {
-		name, mock := name, mock
-		t.Run(name, func(t *testing.T){
-			actualresult := getOpenebsEnv(mock.inputkey, mock.fallbackvalue)
-			if mock.returnvalue != actualresult {
-				t.Fatalf("Test %q failed: expected value=%q, actual value=%q ", name, mock.returnvalue, actualresult)
-			}
-		})
-	//Unset the Environment variable used for unit test
-	os.Unsetenv("_UTEST_OPENEBS")
-	}
-
+  for name, mock := range tests {
+    name, mock := name, mock
+    t.Run(name, func(t *testing.T){
+      actualresult := getOpenebsEnv(mock.inputkey, mock.fallbackvalue)
+      if mock.returnvalue != actualresult {
+        t.Fatalf("Test %q failed: expected value=%q, actual value=%q ", name, mock.returnvalue, actualresult)
+      }
+    })
+  //Unset the Environment variable used for unit test
+  os.Unsetenv("_UTEST_OPENEBS")
+  }
 }
