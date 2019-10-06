ca1c3eb5eb4b465c45ecd273e74d0eeb3fa31aed
Chandan Kumar <chandan.kumar@mayadata.io>
2019-10-06 20:50:02 +0530

    (refactor)BCH: Refactor the exporter logic a/c BCH (#22)
    * Refactor the exporter logic a/c BCH
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Refactor main function
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Refactor getopenebs version function
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Split the chaosResult calculation function
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Add function for populate default value of chaos result
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Refactor exporter function
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Fix the codacy/pr-quality-review and travis/ci issue
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * refactor setChaosResultValue function
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * refactor GetLitmusChaosMetrics function
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Refactor getKubeConfig function to get envs config
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Fix the nil pointer exception
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>
    
    * Fix the BDD test
    
    Signed-off-by: chandan kumar <chandan.kumar@mayadata.io>

---
+++ controller/scrape.go
@@ -0,0 +1,87 @@
+package controller
+
+import (
+	"fmt"
+	"strings"
+
+	// auth for gcp: optional
+	//_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	clientV1alpha1 "github.com/litmuschaos/chaos-operator/pkg/client/clientset/versioned"
+	//"github.com/litmuschaos/chaos-operator/pkg/client/clientset/versioned"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Holds list of experiments in a chaosengine
+var chaosExperimentList []string
+
+// Holds a lookup of result: numericValue
+var numericStatus = map[string]float64{
+	"not-executed": 0,
+	"running":      1,
+	"fail":         2,
+	"pass":         3,
+}
+
+// Utility fn to return numeric value for a result
+func statusConversion(expStatus string) (numeric float64) {
+	if numeric, ok := numericStatus[expStatus]; ok {
+		return numeric
+	}
+	return numericStatus["not-executed"]
+}
+
+// GetLitmusChaosMetrics returns chaos metrics for a given chaosengine
+func GetLitmusChaosMetrics(clientSet *clientV1alpha1.Clientset, exporterSpec ExporterSpec) (float64, float64, float64, map[string]float64, error) {
+	engine, err := clientSet.LitmuschaosV1alpha1().ChaosEngines(exporterSpec.AppNS).Get(exporterSpec.ChaosEngine, metav1.GetOptions{})
+	if err != nil {
+		return 0, 0, 0, nil, err
+	}
+
+	for _, element := range engine.Spec.Experiments {
+		chaosExperimentList = append(chaosExperimentList, element.Name)
+	}
+
+	// Set default values on the chaosResult map before populating w/ actual values
+	spec := ChaosResultSpec{ExporterSpec: exporterSpec, ChaosExperimentList: chaosExperimentList}
+	chaosResultMap := setChaosResultValue(clientSet, spec)
+
+	chaosResult, StatusMap := calculateChaosResult(chaosResultMap)
+	fmt.Printf("%+v\n", StatusMap)
+	totalExpCount := float64(len(engine.Spec.Experiments))
+
+	return totalExpCount, chaosResult.TotalPassedExp, chaosResult.TotalFailedExp, StatusMap, nil
+}
+
+//setChaosResultValue will populate the default value of chaos result
+func setChaosResultValue(clientSet *clientV1alpha1.Clientset, chaosResultSpec ChaosResultSpec) map[string]string {
+	chaosResultMap := make(map[string]string)
+	for _, test := range chaosResultSpec.ChaosExperimentList {
+		chaosResultName := fmt.Sprintf("%s-%s", chaosResultSpec.ExporterSpec.ChaosEngine, test)
+		testResultDump, err := clientSet.LitmuschaosV1alpha1().ChaosResults(chaosResultSpec.ExporterSpec.AppNS).Get(chaosResultName, metav1.GetOptions{})
+		if err != nil {
+			if strings.Contains(err.Error(), "not found") {
+				// lack of result cr indicates experiment not executed
+				chaosResultMap[test] = "not-executed"
+			}
+			continue
+		}
+		chaosResultMap[test] = testResultDump.Spec.ExperimentStatus.Verdict
+	}
+	return chaosResultMap
+}
+
+// calculateChaosResult will calculate the number of pass and failed experiments
+func calculateChaosResult(chaosResult map[string]string) (ChaosExpResult, map[string]float64) {
+	var cr ChaosExpResult
+	StatusMap := make(map[string]float64)
+	for index, status := range chaosResult {
+		if status == "pass" {
+			cr.TotalPassedExp++
+		} else if status == "fail" {
+			cr.TotalFailedExp++
+		}
+		StatusMap[index] = statusConversion(status)
+	}
+
+	return cr, StatusMap
+}
---
+++ controller/types.go
@@ -0,0 +1,78 @@
+package controller
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var registeredResultMetrics []string
+
+// Declare the fixed chaos metrics. Dynamic (testStatus) metrics are defined in metrics()
+var (
+	ExperimentsTotal = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "c",
+		Subsystem: "engine",
+		Name:      "experiment_count",
+		Help:      "Total number of experiments executed by the chaos engine",
+	},
+		[]string{"app_uid", "engine_name", "kubernetes_version", "openebs_version"},
+	)
+
+	PassedExperiments = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "c",
+		Subsystem: "engine",
+		Name:      "passed_experiments",
+		Help:      "Total number of passed experiments",
+	},
+		[]string{"app_uid", "engine_name", "kubernetes_version", "openebs_version"},
+	)
+
+	FailedExperiments = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "c",
+		Subsystem: "engine",
+		Name:      "failed_experiments",
+		Help:      "Total number of failed experiments",
+	},
+		[]string{"app_uid", "engine_name", "kubernetes_version", "openebs_version"},
+	)
+)
+
+// ExporterSpec contains the exporter related specs
+type ExporterSpec struct {
+	ChaosEngine      string
+	AppUUID          string
+	AppNS            string
+	OpenebsNamespace string
+}
+
+// Version contains the version related information
+type Version struct {
+	KubernetesVersion string
+	OpenebsVersion    string
+}
+
+// ExporterConfig contains the config for exporter function
+type ExporterConfig struct {
+	Spec    ExporterSpec
+	version Version
+}
+
+// ChaosResultSpec contains the specs related to generate teh chaos result
+type ChaosResultSpec struct {
+	ExporterSpec        ExporterSpec
+	ChaosExperimentList []string
+}
+
+// ChaosMetricsSpec contains the specs related to chaos metrics
+type ChaosMetricsSpec struct {
+	ExpTotal       float64
+	PassTotal      float64
+	FailTotal      float64
+	ExperimentList map[string]float64
+}
+
+// ChaosExpResult contains the structure of Chaos Result
+type ChaosExpResult struct {
+	TotalExpCount  float64
+	TotalPassedExp float64
+	TotalFailedExp float64
+}
