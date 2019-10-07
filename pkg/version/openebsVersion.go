2eec42fe70164283374270a3a52b67dd126842b1
rampreeth ethiraj <ethirajrampreeth@gmail.com>
2019-10-07 12:23:04 +0530

    Refactor pkg/version/openebsVersion.go
    Signed-off-by: rampreeth ethiraj <ethirajrampreeth@gmail.com>

--- pkg/version/openebsVersion.go
+++ pkg/version/openebsVersion.go
@@ -3,6 +3,7 @@ package version
 import (
 	log "github.com/Sirupsen/logrus"
 	"k8s.io/api/core/v1"
+	"k8s.io/client-go/rest"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
