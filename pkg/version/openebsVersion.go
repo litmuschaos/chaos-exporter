c9abcfed3875ea515941f07f58dca9a31edf9827
rampreeth ethiraj <ethirajrampreeth@gmail.com>
2019-10-07 14:10:09 +0530

    Refactor pkg/version/openebsVersion.go
    Signed-off-by: rampreeth ethiraj <ethirajrampreeth@gmail.com>

--- pkg/version/openebsVersion.go
+++ pkg/version/openebsVersion.go
@@ -7,11 +7,11 @@ import (
 	"fmt"
 )
 
-var openebsVersion = ""
+var openebsVersion string
 var openebsLabel = "openebs.io/component-name=maya-apiserver"
 
-// ObtainList fetches the list of pods
-func ObtainList(clientSet *kubernetes.Clientset, namespace string) (*v1.PodList, error) {
+// getPodList fetches the list of pods
+func getPodList(clientSet *kubernetes.Clientset, namespace string) (*v1.PodList, error) {
 	list, err := clientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{
 		LabelSelector: openebsLabel,
 		Limit:         1,
@@ -19,21 +19,16 @@ func ObtainList(clientSet *kubernetes.Clientset, namespace string) (*v1.PodList,
 	return list, err
 }
 
-// CheckIfEmptyList checks if the obtained pod list is empty
-func CheckIfEmptyList(list *v1.PodList) bool {
-	return len(list.Items) == 0
-}
-
 // GetOpenebsVersion function fetchs the OpenEBS version
 func GetOpenebsVersion(clientSet *kubernetes.Clientset, namespace string) (string, error) {
-	list, err := ObtainList(clientSet, namespace)
+	podList, err := getPodList(clientSet, namespace)
 	if err != nil {
 		return openebsVersion, fmt.Errorf("unable to find openebs/maya api-server %s", err)
 	}
-	if CheckIfEmptyList(list) {
+	if len(podList.Items) == 0 {
 		return openebsVersion, fmt.Errorf("no resources with labels 'openebs.io/component-name=maya-apiserver' found")
 	}
-	for _, v := range list.Items {
+	for _, v := range podList.Items {
 		openebsVersion = v.GetLabels()["openebs.io/version"]
 	}
 	return openebsVersion, err
