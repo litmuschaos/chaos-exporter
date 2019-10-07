ff687c1887ea958d03d5958437204a0ea4717c19
rampreeth <ethirajrampreeth@gmail.com>
2019-10-07 19:22:04 +0530

    (fix) Refactor pkg/version/openebsVersion.go (#25)
    * Refactor pkg/version/openebsVersion.go
    
    Signed-off-by: Rampreeth Ethiraj <ethirajrampreeth@gmail.com>

--- pkg/version/openebsVersion.go
+++ pkg/version/openebsVersion.go
@@ -1,22 +1,32 @@
 package version
 
 import (
-	"fmt"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"fmt"
 )
 
 var openebsVersion string
 var openebsLabel = "openebs.io/component-name=maya-apiserver"
 
+// getPodList fetches the list of pods
+func getPodList(clientSet *kubernetes.Clientset, namespace string) (*v1.PodList, error) {
+	list, err := clientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{
+		LabelSelector: openebsLabel,
+		Limit:         1,
+	})
+	return list, err
+}
+
 // GetOpenebsVersion function fetchs the OpenEBS version
 func GetOpenebsVersion(clientSet *kubernetes.Clientset, namespace string) (string, error) {
-	podList, err := clientSet.CoreV1().Pods(namespace).List(metav1.ListOptions{LabelSelector: openebsLabel, Limit: 1})
+	podList, err := getPodList(clientSet, namespace)
 	if err != nil {
-		return "", fmt.Errorf("unable to find openebs/maya api-server %s", err)
+		return openebsVersion, fmt.Errorf("unable to find openebs/maya api-server %s", err)
 	}
 	if len(podList.Items) == 0 {
-		return "", fmt.Errorf("no resources with labels 'openebs.io/component-name=maya-apiserver' found")
+		return openebsVersion, fmt.Errorf("no resources with labels 'openebs.io/component-name=maya-apiserver' found")
 	}
 	for _, v := range podList.Items {
 		openebsVersion = v.GetLabels()["openebs.io/version"]
