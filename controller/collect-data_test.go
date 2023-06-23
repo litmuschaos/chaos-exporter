a44e1ec0a8389d1229116e315f184b2b956ccdfb
nagesh bansal <nageshbansal59@gmail.com>
2023-06-24 00:50:59 +0530

    Add tests for GetExperimentMetricsFromResult()
    Signed-off-by: nagesh bansal <nageshbansal59@gmail.com>

--- controller/collect-data_test.go
+++ controller/collect-data_test.go
@@ -78,6 +78,95 @@ func TestGetResultList(t *testing.T) {
 	}
 }
 
+func TestGetExperimentMetricsFromResult(t *testing.T) {
+	FakeEngineName := "Fake Engine"
+	FakeNamespace := "Fake Namespace"
+	fakeServiceAcc := "Fake Service Account"
+	fakeAppLabel := "Fake Label"
+	fakeAppKind := "Fake Kind"
+
+	tests := map[string]struct {
+		chaosengine     *v1alpha1.ChaosEngine
+		chaosresult     *v1alpha1.ChaosResult
+		expectedVerdict bool
+		isErr           bool
+	}{
+		"Test Positive-1": {
+			chaosengine: &v1alpha1.ChaosEngine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      FakeEngineName,
+					Namespace: FakeNamespace,
+				},
+				Spec: v1alpha1.ChaosEngineSpec{
+					ChaosServiceAccount: fakeServiceAcc,
+					Appinfo: v1alpha1.ApplicationParams{
+						Appns:    FakeNamespace,
+						Applabel: fakeAppLabel,
+						AppKind:  fakeAppKind,
+					},
+					Experiments: []v1alpha1.ExperimentList{
+						{
+							Name: "Fake-Exp-Name",
+						},
+					},
+				},
+				Status: v1alpha1.ChaosEngineStatus{
+					EngineStatus: v1alpha1.EngineStatusCompleted,
+					Experiments: []v1alpha1.ExperimentStatuses{
+						{
+							Name:   "Fake-Exp-Name",
+							Status: v1alpha1.ExperimentStatusRunning,
+						},
+					},
+				},
+			},
+			chaosresult: &v1alpha1.ChaosResult{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      FakeEngineName + "-" + "Fake-Exp-Name",
+					Namespace: FakeNamespace,
+					UID:       "Fake-UID",
+				},
+				Spec: v1alpha1.ChaosResultSpec{
+					EngineName:     FakeEngineName,
+					ExperimentName: "Fake-Exp-Name",
+				},
+				Status: v1alpha1.ChaosResultStatus{
+					ExperimentStatus: v1alpha1.TestStatus{
+						Phase:   "Completed",
+						Verdict: "Pass",
+					},
+					History: &v1alpha1.HistoryDetails{},
+				},
+			},
+			isErr: false,
+		},
+	}
+
+	for name, mock := range tests {
+		t.Run(name, func(t *testing.T) {
+
+			client := CreateFakeClient(t)
+
+			_, err := client.LitmusClient.LitmuschaosV1alpha1().ChaosEngines(mock.chaosengine.Namespace).Create(context.Background(), mock.chaosengine, metav1.CreateOptions{})
+			if err != nil {
+				t.Fatalf("engine not created for %v test, err: %v", name, err)
+			}
+
+			_, err = client.LitmusClient.LitmuschaosV1alpha1().ChaosResults(mock.chaosresult.Namespace).Create(context.Background(), mock.chaosresult, metav1.CreateOptions{})
+			if err != nil {
+				t.Fatalf("chaosresult not created for %v test, err: %v", name, err)
+			}
+
+			resultDetails := &ChaosResultDetails{}
+			_, err = resultDetails.getExperimentMetricsFromResult(mock.chaosresult, client)
+
+			if !mock.isErr && err != nil {
+				t.Fatalf("Test %q failed: expected error to be nil", name)
+			}
+		})
+	}
+}
+
 func CreateFakeClient(t *testing.T) clients.ClientSets {
 	cs := clients.ClientSets{}
 	cs.KubeClient = fake.NewSimpleClientset([]runtime.Object{}...)
